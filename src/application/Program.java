f53f12b2c7cbeb95082c4975085ad10c4c537b24
Aurea Montero <monteroaurea@gmail.com>
2023-06-21 10:45:20 -0300

    Reading Files simple way

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,20 +8,9 @@ public class Program {
 
 	public static void main(String[] args) {
 		String path = "c:\\temp\\in.txt";
-		
-		// Abrindo e fechando manualmente os streamers:
-		
-		//1. Declarar os streamers
-		
-		FileReader fr = null;
-		BufferedReader br = null;
-		
-		//2. Instanciar os streamers
-	try {
-		fr = new FileReader(path);
-		br = new BufferedReader(fr);
-		
-		//3. Utilizar os streamers
+
+	try (BufferedReader br = new BufferedReader(new FileReader(path))){
+
 		String line = br.readLine();
 		
 		while (line != null) {
@@ -29,28 +18,10 @@ public class Program {
 			line = br.readLine();
 		}
 	}
-		//3. Capturar excessoes
 	
 	catch (IOException e) {
 		System.out.println("Error: " + e.getMessage());
 	}
-		//4. Finally para fechar os streamers
-	
-		finally {
-			try {
-				if (br != null) {
-					br.close();
-				}
-				if (fr != null) {
-					fr.close();
-				}
-			}
-		//5. Capturar excessões do fechamento dos streamers
-			
-			catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
 	}
 	
 }
